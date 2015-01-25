509556ee4ed1ad757308448f7f578cd8d7d99f2a
paduano <auerbach98>
2015-01-25 14:10:08 -0600

    comment

--- js/demos/lineset/DemoLineSet.js
+++ js/demos/lineset/DemoLineSet.js
@@ -212,5 +212,5 @@ function DemoLineSet (containerSvg) {
 
 //PARAMS
 DemoLineSet.demoTitle = "LineSet Demo";
-DemoLineSet.demoDescription = "Implementation of the LineSet visualization for highlight how different pathways share protein and complexes" ;
+DemoLineSet.demoDescription = "Implementation of the LineSet visualization for highlight how different pathways share proteins and complexes" ;
 DemoLineSet.theme = "dark-theme";
